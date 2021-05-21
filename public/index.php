b010a4d71b77e443164de19209ac88a975068e69
Lucas Delobelle <lucas@ld-web.net>
2021-05-21 16:13:31 +0200

    Create User in index.php, filter images requests

--- public/index.php
+++ public/index.php
@@ -3,6 +3,12 @@
 // Composer va installer la méthode de chargement PSR-4 auprès de PHP
 require_once __DIR__ . '/../vendor/autoload.php';
 
+if (php_sapi_name() !== 'cli' && preg_match('/\.(?:png|jpg|jpeg|gif|ico)$/', $_SERVER['REQUEST_URI'])) {
+  // On demande à PHP de servir le fichier demandé directement
+  return false;
+}
+
+use App\Entity\User;
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 
@@ -28,3 +34,14 @@ $config = Setup::createAnnotationMetadataConfiguration(
 );
 
 $entityManager = EntityManager::create($dbParams, $config);
+
+$user = new User();
+$user->setName("Bob");
+
+// Persist permet uniquement de dire au gestionnaire d'entités de gérer l'entité passée en paramètre
+// Persist ne déclenche pas automatiquement une insertion
+$entityManager->persist($user);
+// Pour déclencher l'insertion, on doit appeler la méthode "flush" sur le gestionnaire d'entités
+$entityManager->flush();
+
+var_dump($user);
