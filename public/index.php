30357810412bdc8e4cecc7a584a3c995b55c3040
Lucas Delobelle <lucas@ld-web.net>
2021-05-21 11:15:06 +0200

    Doctrine ORM + console configuration

--- public/index.php
+++ public/index.php
@@ -2,3 +2,29 @@
 
 // Composer va installer la méthode de chargement PSR-4 auprès de PHP
 require_once __DIR__ . '/../vendor/autoload.php';
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+// Indique à Doctrine dans quel dossier aller chercher & analyser les entités
+$paths = [__DIR__ . '/../src/Entity'];
+$isDevMode = true;
+
+$dbParams = [
+  'driver'   => 'pdo_mysql',
+  'host'     => 'localhost',
+  'port'     => '3640',
+  'user'     => 'root',
+  'password' => 'mysqltests',
+  'dbname'   => 'php_mvc'
+];
+
+$config = Setup::createAnnotationMetadataConfiguration(
+  $paths,
+  $isDevMode,
+  null,
+  null,
+  false
+);
+
+$entityManager = EntityManager::create($dbParams, $config);
